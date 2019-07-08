ded9842103ea3e0e5cc6a539337fdac08c5a1d17
realrlgus <rkgus102@gmail.com>
2019-07-09 00:03:36 +0900

    webpack

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ import { localsMiddleware } from "./middlewares";
 const app = express();
 app.use(helmet());
 app.set("view engine", "pug");
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- models/Comment.js
+++ models/Comment.js
@@ -15,6 +15,6 @@ const CommentSchema = new mongoose.Schema({
   }
 });
 
-const model = mongoose.model("Comment", "CommentSchema");
+const model = mongoose.model("Comment", CommentSchema);
 
 export default model;
