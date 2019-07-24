35e50f7d7c03f76c59de5af09bde5282741b7c0e
realrlgus <rkgus102@gmail.com>
2019-07-25 00:33:58 +0900

    Add Remove Comments!!

--- assets/js/removeComment.js
+++ assets/js/removeComment.js
@@ -3,20 +3,26 @@ import axios from "axios";
 const removeCommentBtn = document.querySelectorAll(".video__comments-remove");
 const commentNumber = document.getElementById("jsCommentNumber");
 
-const removeComment = async id => {
-  await axios({
+const removeList = li => {
+  li.remove();
+  commentNumber.innerHTML = commentNumber.innerHTML * 1 - 1;
+};
+
+const removeComment = async (id, list) => {
+  const response = await axios({
     url: `/api/${id}/removeComment`,
     method: "POST",
     data: {
       id
     }
   });
+  if (response.status === 200) {
+    removeList(list);
+  }
 };
 
 const handleClick = e => {
-  e.path[1].remove();
-  commentNumber.innerHTML = commentNumber.innerHTML * 1 - 1;
-  removeComment(e.path[0].getAttribute("data"));
+  removeComment(e.path[0].getAttribute("data"), e.path[1]);
 };
 
 function init() {
